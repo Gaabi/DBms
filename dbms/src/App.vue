2bb4fd16682f5deceac4232d10b16d2d5390bc93
Gaabi <garimasoni.pesit@gmail.com>
2017-11-25 17:32:23 +0530

    'trying'

---
+++ dbms/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+   <img src ="./assets/pesit_logo.png" align=left  > 
+   <userend> </userend>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import userend from './components/userend'
+export default {
+  name: 'app',
+  components :{
+    userend
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color:#33C1FF  ;
+
+}
+</style>
---
+++ dbms/src/components/userend.vue
@@ -0,0 +1,55 @@
+<template>
+<div id="user" class="user">
+<title>Demand Forecasting</title>
+<h1 align=center>Required books in library</h1>
+<h2 align=left>Input date</h2>
+<p align =left>
+</hr>
+<label><b>FROM DATE </b>    </label><input type="date" placeholder="mm/dd/yyyy" ></br></br></br>
+<label><b>TO DATE    : </b> </label><input type ="date"></br></br></br>
+<button class="button" v-on:click="clicked">Submit </button>
+</p>
+<p> <h3><b >ANALYSIS</b></h3></p>
+</hr>
+
+</div>
+</template>
+
+<script>
+export default {
+name :'user',
+method :{
+    clicked:function(){
+        alert ("instead of alert data will be displayed");
+    }
+}
+
+}
+</script>
+
+
+<style>
+.button{
+    background-color:#154AA7   ;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+#user{
+font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+   color: #2c3e50;
+
+  margin-left:60px;
+   background:#33C1FF  ;
+  
+}
+</style>
